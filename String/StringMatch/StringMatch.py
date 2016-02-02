0bbd0d005df3c93c8ac9b6e82fe925b4375d2aa7
baiyp <baiyp@Ctrip.com>
2016-02-02 13:36:23 +0800

    Implement KMP algorithm

--- String/StringMatch/StringMatch.py
+++ String/StringMatch/StringMatch.py
@@ -31,22 +31,52 @@ def algorithm1(src, target):
     return -1
 
 
+def make_next(target):
+    l = [0] * len(target)
+    flag = 0
+    for i in range(1, len(target)):
+        if target[i] == target[flag]:
+            flag += 1
+        else:
+            flag = 0
+        l[i] = flag
+
+    return l
+
+
 # KMP 算法
 def algorithm2(src, target):
-    pass
+    l = make_next(target)
+    target_length = len(target)
+    src_length = len(src)
+    src_index = 0
+    target_index = 0
+    while src_index < src_length:
+        while target_index < target_length:
+            if src_index + target_index >= src_length:
+                return -1
+            if src[src_index + target_index] == target[target_index]:
+                target_index += 1
+            elif target_index >= 1:
+                src_index += target_index - l[target_index - 1]
+                target_index = l[target_index - 1]
+            else:
+                src_index += 1
+                target_index = 0
+
+        if target_index == target_length:
+            return src_index
+
+    return -1
 
 
 if __name__ == "__main__":
-
     src = random_string(10000)
     print datetime.datetime.now()
-    for i in range(10000):
-        target = random_string(20)
+    for i in range(1000):
+        target = random_string(5)
 
         try:
-            if src.find(target) != -1:
-                print src
-                print target
             if src.find(target) != algorithm1(src, target):
                 print "Test Failed"
                 print src
@@ -57,3 +87,5 @@ if __name__ == "__main__":
             print target
 
     print datetime.datetime.now()
+    # print algorithm2("abcaabcaabcab", "abcab")
+    # print algorithm2("abcaabcaabcaa", "abcab")
